789ef9a56e6f13c444537d3b1dea8e50e8fffc0d
ImadEddineBek <bek34046629@yahoo.com>
2017-10-31 21:31:42 +0100

    Last Version

--- feature/src/main/java/com/imadeddine/bekkouche/howmuchdoyouweighonmars/feature/MainActivity.kt
+++ feature/src/main/java/com/imadeddine/bekkouche/howmuchdoyouweighonmars/feature/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.imadeddine.bekkouche.howmuchdoyouweighonmars.feature
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        editText.text
+        val samta= editText.text.toString()
+        editText.addTextChangedListener(TextChangeListener(textView3,samta))
+
     }
+}
+class TextChangeListener(private var textView:TextView, private var samta:String):TextWatcher{
+
+    override fun afterTextChanged(s: Editable?) {
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        if (s.toString() != samta)
+        textView.text= (s.toString().toInt()*0.38).toString()
+        else textView.text=""
+    }
+
 }
